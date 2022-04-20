a9310f016f730b8df5dcba21d427dbda8628e4bd
rumeysa-akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-04-20 13:00:30 +0300

    Add project files.

---
+++ Stoneity Case/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//case
+//int[] sayilar = { 4, 13, 8, 9, 7, 1, 6 };
+int[] sayilar = { 16, 23, 7, 11, 3, 14};
+Array.Sort(sayilar);
+Array.Reverse(sayilar);
+for (int i = 0; i < sayilar.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(sayilar[i]);
+    }
+}
+for (int i = sayilar.Length -1; i >=0;i--)
+{
+    if (i % 2 == 1)
+    {
+        Console.WriteLine(sayilar[i]);
+    }
+}
+
+
+
+
